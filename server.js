4895894ff0571cf4989d07825726d1ac5b454668
kotkotenok45 <kotkotenok43434343@gmail.com>
2026-03-11 13:15:49 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        const data = JSON.parse(message);
+
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(data));
+            }
+        });
+    });
+});
+
+console.log('Kotochat Server запущен на порту 8080');
